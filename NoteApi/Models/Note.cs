04a269d41a520bc0bcfe1b48c66f5e0a5d6b0a67
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-04 09:59:56 +0200

    add Note Model and make it as domai model in DBContext

---
+++ NoteApi/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NoteApi.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [MaxLength(2500)]
+        public string Content { get; set; }
+
+        public DateTime NoteId { get; set; }
+
+    }
+}
